df8d4c40eee10627baa8cb17ab75bd5385987b47
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-06 00:36:09 +0200

    fix: :bug: only answer to GET requests

--- main.c
+++ main.c
@@ -79,60 +79,61 @@ int main(void) {
     fgets(recvline, MAX_LINE - 1, stream);
     sscanf(recvline, "%s %s %s\n", method, uri, version);
 
-    static char moduri[MAX_PROP_LEN];
+    if (strcmp(method, "GET") == 0) {
+      static char moduri[MAX_PROP_LEN];
 
-    memset(moduri, 0, MAX_PROP_LEN);
-    strcpy(moduri, "./" EXPOSED_FOLDER);
-    strcat(moduri, uri);
-
-    int statuscode = 200;
-
-    // check if file or directory does exist
-    if (stat(moduri, &sbuf) < 0) {
-      // if it doesn't try with an html extension
       memset(moduri, 0, MAX_PROP_LEN);
       strcpy(moduri, "./" EXPOSED_FOLDER);
       strcat(moduri, uri);
-      strcat(moduri, ".html");
-    } else {
-      // if it does check if is not a file
-      if (!S_ISREG(sbuf.st_mode)) {
-        // if it is not a directory append an index.html at the end
-        if (moduri[strlen(moduri) - 1] != '/')
-          strcat(moduri, "/");
-        strcat(moduri, "index.html");
+
+      int statuscode = 200;
+
+      // check if file or directory does exist
+      if (stat(moduri, &sbuf) < 0) {
+        // if it doesn't try with an html extension
+        memset(moduri, 0, MAX_PROP_LEN);
+        strcpy(moduri, "./" EXPOSED_FOLDER);
+        strcat(moduri, uri);
+        strcat(moduri, ".html");
+      } else {
+        // if it does check if is not a file
+        if (!S_ISREG(sbuf.st_mode)) {
+          // if it is not a directory append an index.html at the end
+          if (moduri[strlen(moduri) - 1] != '/')
+            strcat(moduri, "/");
+          strcat(moduri, "index.html");
+        }
       }
+
+      // check again whether the file exists
+      if (stat(moduri, &sbuf) < 0) {
+        // if it doesn't change the path to the 404 page
+        statuscode = 404;
+        fprintf(stderr, "[ERROR] 404 File %s does not exist\n", moduri);
+        memset(moduri, 0, MAX_PROP_LEN);
+        strcpy(moduri, "./" EXPOSED_FOLDER);
+        strcat(moduri, "/404.html");
+      }
+
+      // check if the 404 page exists if not exit the program
+      if (stat(moduri, &sbuf) < 0) {
+        fprintf(stderr, "[ERROR] 404 page does not exist\n");
+        exit(EXIT_FAILURE);
+      }
+
+      fprintf(stream, "HTTP/1.1 %d OK\n", statuscode);
+      fprintf(stream, "Server: serverfromscratch\n");
+      fprintf(stream, "Content-Type: text/html\n");
+      fprintf(stream, "\r\n\r\n");
+      fflush(stream);
+
+      int fd = open(moduri, O_RDONLY);
+      void *p = mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+      fwrite(p, 1, sbuf.st_size, stream);
+      munmap(p, sbuf.st_size);
     }
-
-    // check again whether the file exists
-    if (stat(moduri, &sbuf) < 0) {
-      // if it doesn't change the path to the 404 page
-      statuscode = 404;
-      fprintf(stderr, "[ERROR] 404 File %s does not exist\n", moduri);
-      memset(moduri, 0, MAX_PROP_LEN);
-      strcpy(moduri, "./" EXPOSED_FOLDER);
-      strcat(moduri, "/404.html");
-    }
-
-    // check if the 404 page exists if not exit the program
-    if (stat(moduri, &sbuf) < 0) {
-      fprintf(stderr, "[ERROR] 404 page does not exist\n");
-      exit(EXIT_FAILURE);
-    }
-
-    fprintf(stream, "HTTP/1.1 %d OK\n", statuscode);
-    fprintf(stream, "Server: serverfromscratch\n");
-    fprintf(stream, "Content-Type: text/html\n");
-    fprintf(stream, "\r\n\r\n");
-    fflush(stream);
-
-    int fd = open(moduri, O_RDONLY);
-    void *p = mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    fwrite(p, 1, sbuf.st_size, stream);
-    munmap(p, sbuf.st_size);
 
     fclose(stream);
-
     close(fdclient);
   }
 }
